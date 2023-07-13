13b3e7de53bafe07cdf232180f2a568dedfcb27d
Manasharadhakrishnan <manumathu0164@gmail.com>
2023-07-13 11:00:13 +0530

    task commited

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import TaskList from './TaskList';
-import AddTaskForm from './AddTaskForm';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Datatable from './Datatable';
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    const storedTasks = localStorage.getItem('tasks');
-    if (storedTasks) {
-      setTasks(JSON.parse(storedTasks));
-    }
-  }, []);
-
-  const addTask = task => {
-    const newTask = { ...task, id: Date.now(), completed: false };
-    const updatedTasks = [...tasks, newTask];
-    setTasks(updatedTasks);
-    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-  };
-
-  const deleteTask = taskId => {
-    if (window.confirm('Are you sure you want to delete this task?')) {
-      const updatedTasks = tasks.filter(task => task.id !== taskId);
-      setTasks(updatedTasks);
-      localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-    }
-  };
-
-  const toggleTaskCompletion = taskId => {
-    const updatedTasks = tasks.map(task =>
-      task.id === taskId ? { ...task, completed: !task.completed } : task
-    );
-    setTasks(updatedTasks);
-    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-  };
-
   return (
-    <div className="container">
-
-      <AddTaskForm onAddTask={addTask} />
-      <TaskList
-        tasks={tasks}
-        onDeleteTask={deleteTask}
-        onToggleTaskCompletion={toggleTaskCompletion}
-      />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/datatable" element={<Datatable />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
